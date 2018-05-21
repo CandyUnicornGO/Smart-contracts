0ece3e6c897bf70bd2023fda19b24cc12cae9c59
Koltsov Anton <79096624730@mail.ru>
2018-05-21 12:21:24 +0300

    1.8

---
+++ Tournament/Tournament.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.21;
+
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+
+contract ERC20 {
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+}
+
+
+
+contract UnicornTournament {
+    using SafeMath for uint;
+    ERC20 public candyToken;
+
+    event HybridizationAccept(uint indexed firstUnicornId, uint indexed secondUnicornId, uint newUnicornId);
+
+
+    struct tournament{
+        uint blockNum;
+
+    }
+
+
+
+    function() public payable {
+
+    }
+
+    function UnicornTournament()  public {
+
+    }
+
+
+
+}
---
+++ old/88.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.21;
+
+contract test {
+    uint public tt;
+function test(){
+  tt = 2;
+}
+}
