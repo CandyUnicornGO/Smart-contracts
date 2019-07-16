989caaef729a7ce9397115ba906fdc8166c5ea3d
dilap54 <dilap54@mail.ru>
2019-07-16 21:51:50 +0300

    Add tourney running algorithm

--- Ethereum/Tournament/Tournament.sol
+++ Ethereum/Tournament/Tournament.sol
@@ -77,7 +77,7 @@ contract UnicornAccessControl {
 
     function UnicornAccessControl(address _unicornManagementAddress) public {
         unicornManagement = UnicornManagementInterface(_unicornManagementAddress);
-        unicornManagement.registerInit(this);
+        //unicornManagement.registerInit(this);
     }
 
     modifier onlyOwner() {
@@ -150,6 +150,10 @@ contract UnicornAccessControl {
     }
 }
 
+contract DividendManagerInterface {
+    function payDividend() external payable;
+}
+
 contract UnicornTokenInterface {
 
     //ERC721
@@ -204,20 +208,63 @@ interface UnicornBalancesInterface {
     function transferWithFee(address _token, address _userFrom, uint _fullPrice, address _feeTaker, address _priceTaker, uint _price) external returns (bool);
 }
 
+/**
+ * Что такое «Турнир»? Это соревнование единорогов, состоящее из 3х этапов (матчей), приз за победу в котором — CandyCoin.
+ * Нельзя совершенно точно знать победителя, можно лишь предположить об этом с долей вероятности.
+ * У каждого Единорога есть 5 базовых характеристик, которые задаются при рождении:
+ * *  Сила (Strength)
+ * *  Проворность (Agility)
+ * *  Скорость (Speed)
+ * *  Интеллект (Intellect)
+ * *  Обаяние (Charm)
+ * Турнир состоит из трех последовательных матчей. Каждый матч имеет главную и второстепенную характеристики,
+ * по которым будут соревноваться единороги. Тип каждого матча выбирается случайно
+ * и может повторяться в рамках турнира. В течении каждого матча происходит
+ * 3 последовательных генерации случайного числа с помощью физических характеристик единорога.
+ * Для этого выбираются первичная и вторичная характеристики в зависимости от типа матча.
+ * Далее происходит генерация случайного числа в интервале от 1 до значения соответствующей характеристики
+ * (например Сила 7, значит интервал [1;7]) и умножается на коэффициент в зависимости
+ * от первичности или вторичности характеристики в данном типе матча. Полученные числа являются количеством очков,
+ * которые набрал Единорог. Победителем в турнире стает тот, у кого наибольшее количество очков за 3 матча
+ * (прим. все очки умножаются на 100 для красоты числа).
+ * Последовательность:
+ * *  Регистрация в турнире
+ * *  Начало турнира
+ * *  Определение джекпота турнира
+ * *  Определение типа матча 1
+ * *  Начало матча 1
+ * *  Конец матча 1
+ * *  Определение типа матча 2
+ * *  Начало матча 2
+ * *  Конец матча 2
+ * *  Определение типа матча 3
+ * *  Начало матча 3
+ * *  Конец матча 3
+ * *  Подсчет очков и определение победителя
+ * *  Выдача награды
+ */
 
 contract UnicornTournament is UnicornAccessControl{
     using SafeMath for uint;
     //ERC20 public candyToken;
 
+    uint8 constant maxTournamentPlayers = 5;
+    uint8 constant numberOfMatches = 3;
+    
+    uint16 MAIN_CHARACTERISTIC_RATIO = 15;
+    uint16 SECONDARY_CHARACTERISTIC_RATIO = 10;
 
     struct Tournament{
-        uint[] unicorns;
+        uint[maxTournamentPlayers] unicorns;
         uint blockNum;
-        uint index;
         bool finished;
+        uint winner;
     }
 
-    mapping(uint => Tournament) public tournaments;
+    uint[maxTournamentPlayers] queue;
+    uint8 public queueLength = 0;
+
+    Tournament[] public tournaments;
     //unicornId => tournamentId
     mapping (uint => uint) public unicornTournament;
     // Tournament index => tournamentId
@@ -225,15 +272,126 @@ contract UnicornTournament is UnicornAccessControl{
     uint public tournamentsSize = 0;
 
     uint tournamentId = 0;
-    uint public maxTournamentPlayers = 5;
+    UnicornTokenInterface unicornToken;
+    TrustedTokenInterface megaCandyToken;
+    UnicornBalancesInterface balances;
 
-    function() public payable {
+    event FundsTransferred(address dividendManager, uint value);
 
+    event rndEvent(uint rnd);
+    event timeEvent(uint time);
+    event kek(bytes32 _bytes);
+    event kek8(byte _byte);
+
+    function test() public returns (uint8)  {
+        uint8 rnd = uint8(uint256(keccak256(block.timestamp, block.difficulty))%10);
+        emit kek(keccak256(block.timestamp, block.difficulty));
+        emit kek8(byte(keccak256(block.timestamp, block.difficulty)<<8));
+        return rnd;
     }
 
+    event queueLengthEvent(uint8 length);
+    //Add unicorn to tournament
+    function participate(uint _unicornId) public returns (uint){
+        emit queueLengthEvent(queueLength);
+        queue[queueLength++] = _unicornId;
+        if (queueLength == maxTournamentPlayers){
+            tournaments.length++;
+            uint tournamentIndex = tournaments.length - 1;
+            tournaments[tournamentIndex].blockNum = block.number;
+            tournaments[tournamentIndex].finished = false;
+            for(uint8 i = 0; i<maxTournamentPlayers; i++){
+                tournaments[tournamentIndex].unicorns[i] = queue[i];
+            }
+            queueLength = 0;
+            emit queueLengthEvent(queueLength);
+            return tournaments.length-1;
+        } else {
+            return tournaments.length;
+        }
+    }
+    
+    uint8 constant STRENGTH_GEN_BYTE = 112;
+    uint8 constant AGILITY_GEN_BYTE = 117;
+    uint8 constant SPEED_GEN_BYTE = 112;
+    uint8 constant INTELLECT_GEN_BYTE = 112;
+    uint8 constant CHARISMA_GEN_BYTE = 112;
+    
+    //Main and secondary characteristic in 3 matches
+    uint8[2][3] mapMatchTypeToGenNumber = [
+        [SPEED_GEN_BYTE, AGILITY_GEN_BYTE],//race
+        [STRENGTH_GEN_BYTE, AGILITY_GEN_BYTE],//fight
+        [CHARISMA_GEN_BYTE, INTELLECT_GEN_BYTE] //pair
+    ];
 
+    event rand4bit(uint8 max, uint8 points);
+    event unicornPoint(uint8 unicornIndex, uint32 unicornId, uint8 max, uint8 points);
+    event pointsEvent(uint16[maxTournamentPlayers]);
+    event matchesTypesEvent(uint8[3]);
+    
+    function runTournament(uint _tournamentId) public{
+        require(tournaments.length > _tournamentId);//Tournament created
+        //require(!tournaments[_tournamentId].finished);//Tournament not finished
+        
+        bytes32 rnd = bytes32(keccak256(block.timestamp, block.difficulty));//random hash
+        uint16[maxTournamentPlayers] memory points; //Unicorn's points
+        
+        ///Matches type generation
+        uint8 matchesRnd = uint8(rnd);//random last 8 bits from hash
+        rnd = rnd >> 8;//delete last 8 bits in hash
+        uint8[3] memory matchedTypes = [
+            matchesRnd%3,
+            matchesRnd/3%3,
+            matchesRnd/9%3
+        ];
+        
+        emit matchesTypesEvent(matchedTypes);
+        
+        uint256 rndInt = uint256(rnd);
+        ///Copmute points
+        for (uint8 matchNumber=0; matchNumber<numberOfMatches; matchNumber++){//Every match
+            for (uint8 unicorn=0; unicorn<maxTournamentPlayers; unicorn++){//Every unicorn
+                //Main characteristic in this match from unicorn
+                uint8 mainCharacteristic = unicornToken.getUnicornGenByte(tournaments[_tournamentId].unicorns[unicorn], mapMatchTypeToGenNumber[matchedTypes[matchNumber]][0]);
+                //Secondary characteristic in this match from unicorn
+                uint8 secondaryCharacteristic = unicornToken.getUnicornGenByte(tournaments[_tournamentId].unicorns[unicorn], mapMatchTypeToGenNumber[matchedTypes[matchNumber]][1]);
+                for (uint8 stepNumber=0; stepNumber<2; stepNumber++){//2 times
+                    /*
+                    uint8 unicornRnd = uint8(rnd << 4) >> 4;//get last 4 bits from hash
+                    rnd = rnd >> 4;// delete last 4 bits in hast
+                    uint256 
+                    emit kek(rnd);
+                    emit rand4bit(unicornRnd);
+                    */
+                    points[unicorn] += uint16(rndInt % mainCharacteristic) * MAIN_CHARACTERISTIC_RATIO;
+                    rndInt = rndInt/mainCharacteristic;
+                    points[unicorn] += uint16(rndInt % secondaryCharacteristic) * MAIN_CHARACTERISTIC_RATIO;
+                    rndInt = rndInt/secondaryCharacteristic;
+                }
+            }
+        }
+        emit pointsEvent(points);
+        
+        uint8 winner = 0;
+        for (uint8 i = 0; i< maxTournamentPlayers; i++){
+            if (points[i] > points[winner]){
+                winner = i;
+            }
+        }
+        
+        tournaments[_tournamentId].winner = tournaments[_tournamentId].unicorns[winner];
+        tournaments[_tournamentId].finished = true;
+    }
 
+    function getTournamentsLength() public view returns(uint){
+        return tournaments.length;
+    }
 
+    function getQueue() public view returns (uint[maxTournamentPlayers]){
+        return queue;
+    }
+
+    /*
     function createTournament(uint _unicornId) public {
         require(unicornToken.owns(msg.sender, _unicornId));
         require(unicornTournament[_unicornId] == 0);
@@ -262,8 +420,10 @@ contract UnicornTournament is UnicornAccessControl{
         }
 
     }
+    */
 
-    function getTournamentUnicorns(uint _tournamentId)  public view returns (uint[]) {
+
+    function getTournamentUnicorns(uint _tournamentId)  public view returns (uint[maxTournamentPlayers]) {
         return tournaments[_tournamentId].unicorns;
     }
 
@@ -273,12 +433,14 @@ contract UnicornTournament is UnicornAccessControl{
     function UnicornTournament(address _balances, address _unicornManagementAddress) UnicornAccessControl(_unicornManagementAddress) public {
         //candyTokenAddress = unicornManagement.candyToken();
         balances = UnicornBalancesInterface(_balances);
+        unicornToken = UnicornTokenInterface(unicornManagement.unicornTokenAddress());
+        //megaCandyToken = TrustedTokenInterface(unicornManagement.candyPowerToken());
     }
 
-    function init() onlyManagement whenPaused external {
+    /*function init() onlyManagement whenPaused external {
         unicornToken = UnicornTokenInterface(unicornManagement.unicornTokenAddress());
         megaCandyToken = TrustedTokenInterface(unicornManagement.candyPowerToken());
-    }
+    }*/
 
 
 
